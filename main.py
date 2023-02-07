3cd746db013547ac0b40c161d3183856724fb517
Shaghayegh <sha.karimi.n@gmail.com>
2023-02-07 17:39:45 +0100

    moving to oop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from turtle import Screen, Turtle
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,26 +9,15 @@ screen.title('Swish Swish Bish')
 # Turning off the tracer
 screen.tracer(0)
 
-segments = []
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
-for position in starting_position:
-    new_segment = Turtle('square')
-    new_segment.color('white')
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     # Snake body will show up in one piece
     screen.update()
     time.sleep(0.1)
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
+
 
 
 
