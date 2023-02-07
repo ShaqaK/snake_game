951250134acf996747eb6154481195e2a3944576
Shaghayegh <sha.karimi.n@gmail.com>
2023-02-07 16:55:46 +0100

    create snake

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('Swish Swish Bish')
+
+
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+for position in starting_position:
+    new_segment = Turtle('square')
+    new_segment.color('white')
+    new_segment.goto(position)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
