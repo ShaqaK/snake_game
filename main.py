01a6d8c66f5945590015467f10bc6a1fefea9a50
Shaghayegh <sha.karimi.n@gmail.com>
2023-02-07 18:43:41 +0100

    collision with food nom nom

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
-
+from food import Food
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
@@ -10,6 +10,7 @@ screen.title('Swish Swish Bish')
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 screen.listen()
 screen.onkey(snake.up, 'Up')
 screen.onkey(snake.down, 'Down')
@@ -22,6 +23,12 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect Collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
+
 
 
 
